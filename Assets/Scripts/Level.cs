ce39301dd0f7280e1a680abf97a858ed01751b75
David Kikozashvili <kikodave@gmail.com>
2019-03-26 18:34:24 -0400

    Added Scenes
    Added Start Menu and Game Over Scenes

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // needs to be changed if more levels added
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    // needs to be changed if more levels added
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
